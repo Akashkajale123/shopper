be6071715d481023dc511ed213f0aaa03979eae9
Akash Kajale <akashkajale125@gmail.com>
2024-01-19 03:49:16 +0530

    sub-pages done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from "./pages/Product.js";
 import Navbar from "./components/Navbar/Navbar.js";
 import Cart from "./pages/Cart.js";
 import Footer from "./components/Footer/Footer.js";
+import men_banner from './Assets/banner_mens.png'
+import women_banner from './Assets/banner_women.png'
+import kids_banner from './Assets/banner_kids.png'
 
 const App = () => {
   return (
@@ -14,9 +17,9 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/mens" element={<ShopCategory category="mens" />} />
-        <Route path="/womens" element={<ShopCategory category="womens" />} />
-        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/mens" element={<ShopCategory category="men" banner={men_banner}/>} />
+        <Route path="/womens" element={<ShopCategory category="women" banner={women_banner} />} />
+        <Route path="/kids" element={<ShopCategory category="kid" banner={kids_banner}/>} />
         <Route path="/product" element={<Product />}>
           <Route path=":productId" element={<Product />} />
         </Route>
