b3f8981bd00e2f767217368e0cafdd3f12ee48a0
Akash Kajale <akashkajale125@gmail.com>
2024-01-20 17:30:43 +0530

    Releted_products done

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,6 +3,8 @@ import { ShopContext } from '../context/ShopContext';
 import { useParams } from 'react-router-dom';
 import BreadCrum from '../components/BreadCrum.js/BreadCrum';
 import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext); // Assuming products are stored in a property named 'products'
@@ -15,6 +17,8 @@ const Product = () => {
     <div>
       <BreadCrum product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox/>
+      <RelatedProducts />
     </div>
   );
 };
